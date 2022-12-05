fc417f97c59fd77e496e70a0eed662cb2890bcc4
Scott Hammer <shammer64@gmail.com>
2022-12-05 18:03:25 -0500

    Final day1 solution

--- src/main/kotlin/dev/scotthammer/advent22/day1/CaloriesCalc.kt
+++ src/main/kotlin/dev/scotthammer/advent22/day1/CaloriesCalc.kt
@@ -1,11 +1,44 @@
 package dev.scotthammer.advent22.day1
 
-class CaloriesCalc(val input: Array<String>) {
-    fun findElfWithMostest(): Long {
-        if (input.isNotEmpty()) {
-            return 1
+class CaloriesCalc(private val input: Array<String>) {
+    private var elves = mutableSetOf<Elf>();
+    private var sortedElves = mutableListOf<Elf>()
+    init {
+        var currElfNum: Long = 1
+        var currElf: Elf = Elf(currElfNum, 0)
+        for (item in input) {
+            if (item.isEmpty()) {
+                currElfNum += 1
+                currElf = Elf(currElfNum, 0)
+            } else {
+                elves.add(currElf)
+                val cals = item.toLong()
+                currElf.calories += cals
+            }
         }
-        return -1
+        sortedElves = elves.toMutableList()
+        sortedElves.sort()
+    }
+
+    fun findElfWithMostest(): Elf? {
+        var maxElf: Elf? = sortedElves.getOrNull(0)
+        return maxElf
+    }
+
+    fun findTop3ElvesWithMostest(): List<Elf> {
+        return listOf(
+            sortedElves[0],
+            sortedElves[1],
+            sortedElves[2]
+        )
+    }
+
+    class Elf(id: Long, calories: Long) : Comparable<Elf> {
+        var id: Long = id
+        var calories: Long = calories
+        override fun compareTo(other: Elf): Int {
+            return if (calories - other.calories <= 0) 1 else -1
+        }
     }
 
 }
--- src/test/kotlin/dev/scotthammer/advent22/day1/CaloriesTest.kt
+++ src/test/kotlin/dev/scotthammer/advent22/day1/CaloriesTest.kt
@@ -1,16 +1,29 @@
 package dev.scotthammer.advent22.day1
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CaloriesTest {
+    var fileInput: Array<String>? = null
+
+    @BeforeAll
+    internal fun setUp() {
+        fileInput = readFromInputFile("/calories_input.txt")
+    }
 
     @Test
     fun shouldHandleEmptyArray() {
         val input = emptyArray<String>()
         val caloriesCalc = CaloriesCalc(input)
 
-        assertEquals(-1, caloriesCalc.findElfWithMostest())
+        val maxElf = caloriesCalc.findElfWithMostest()
+
+        assertNull(maxElf)
     }
 
     @Test
@@ -18,7 +31,72 @@ class CaloriesTest {
         val input = arrayOf<String>("1000")
         val caloriesCalc = CaloriesCalc(input)
 
-        assertEquals(1, caloriesCalc.findElfWithMostest())
+        val maxElf = caloriesCalc.findElfWithMostest()
+
+        assertNotNull(maxElf)
+        assertEquals(1, maxElf.id)
+        assertEquals(1000, maxElf.calories)
+    }
+
+    @Test
+    fun shouldHandleTwoElfSingleItemArray() {
+        val input = arrayOf<String>("1000", "", "2000")
+        val caloriesCalc = CaloriesCalc(input)
+
+        val maxElf = caloriesCalc.findElfWithMostest()
+
+        assertNotNull(maxElf)
+        assertEquals(2, maxElf.id)
+        assertEquals(2000, maxElf.calories)
+    }
+
+    @Test
+    fun shouldHandleExerciseExample() {
+        val input = arrayOf<String>(
+            "1000", "2000", "",
+            "4000", "",
+            "5000", "6000", "",
+            "7000", "8000", "9000", "",
+            "10000"
+        )
+        val caloriesCalc = CaloriesCalc(input)
+
+        val maxElf = caloriesCalc.findElfWithMostest()
+
+        assertNotNull(maxElf)
+        assertEquals(4, maxElf.id)
+        assertEquals(24000, maxElf.calories)
+    }
+
+    @Test
+    fun shouldHandleLargeArrayFromFile() {
+        val caloriesCalc = fileInput?.let { CaloriesCalc(it) }
+
+        val maxElf = caloriesCalc?.findElfWithMostest()
+
+        assertNotNull(maxElf)
+        assertEquals(7, maxElf.id)
+        assertEquals(67633, maxElf.calories)
+    }
+
+    @Test
+    fun shouldHandleLargeArrayFromFileTop3() {
+        val caloriesCalc = fileInput?.let { CaloriesCalc(it) }
+
+        val maxElves = caloriesCalc?.findTop3ElvesWithMostest()
+
+        assertNotNull(maxElves)
+        assertEquals(7, maxElves[0].id)
+        assertEquals(67633, maxElves[0].calories)
+        assertEquals(149, maxElves[1].id)
+        assertEquals(66296, maxElves[1].calories)
+        assertEquals(158, maxElves[2].id)
+        assertEquals(65699, maxElves[2].calories)
+    }
+
+    private fun readFromInputFile(inputFileName: String): Array<String> {
+        return this::class.java.getResourceAsStream(inputFileName)
+            .bufferedReader().readLines().toTypedArray()
     }
 
 }
